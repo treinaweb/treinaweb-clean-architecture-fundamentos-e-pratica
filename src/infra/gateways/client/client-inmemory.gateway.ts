3a87d9ff7bef9b853a3a73871b71a176e187bc0c
Cleyson Lima <cleysonph@gmail.com>
2022-08-18 21:16:21 -0300

    04:12 - criando uma implementação de Client Gateway em memória

---
+++ src/infra/gateways/client/client-inmemory.gateway.ts
@@ -0,0 +1,18 @@
+import { ClientGateway } from "../../../application/gateways/client/client.gateway";
+import { Client } from "../../../enterprise/entities/client/client.entity";
+
+export class ClientInMemoryGateway implements ClientGateway {
+  private static clients: Client[] = [
+    new Client("Test", "Test", "test@mail.com", "12345678912"),
+  ];
+
+  async findAll(): Promise<Client[]> {
+    let clonedClients: Client[] = [];
+    ClientInMemoryGateway.clients.forEach((client) =>
+      clonedClients.push(
+        new Client(client.firstName, client.lastName, client.email, client.cpf)
+      )
+    );
+    return clonedClients;
+  }
+}
--- tests/application/usecases/client/find-all-clients.usecase.test.ts
+++ tests/application/usecases/client/find-all-clients.usecase.test.ts
@@ -1,17 +1,9 @@
 import { FindAllClientsUseCase } from "../../../../src/application/usecases/client/find-all-clients.usecase";
-import { ClientGateway } from "../../../../src/application/gateways/client/client.gateway";
 import { Client } from "../../../../src/enterprise/entities/client/client.entity";
-
-class ClientGatewayMock implements ClientGateway {
-  async findAll(): Promise<Client[]> {
-    return Promise.resolve([
-      new Client("Test", "Test", "test@mail.com", "12345678912"),
-    ]);
-  }
-}
+import { ClientInMemoryGateway } from "../../../../src/infra/gateways/client/client-inmemory.gateway";
 
 test("deve return a lista de clientes", async () => {
-  const clientGateway = new ClientGatewayMock();
+  const clientGateway = new ClientInMemoryGateway();
   const findAllClientsUseCase = new FindAllClientsUseCase(clientGateway);
 
   const expectedClients = [
